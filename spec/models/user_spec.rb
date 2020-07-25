2b894d896e08d0e242b66f6ca4dc9c240ff44b92
silverskg <chokichoki_gm@i.softbank.jp>
2020-07-25 15:25:54 +0900

    first commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "名前の表示" do
+  #   it "年齢1:ユーザーの名前が取得できること"do
+  #     user = User.new(name: "satou",age: 1)
+  #     expect(user.disp_name).to eq "satouちゃん"
+  # end
+  #   it "年齢15:ユーザーの名前が取得できること" do
+  #     user = User.new(name: "satou", age: 15)
+  #     expect(user.disp_name).to eq "satouくん"
+  # end
+  #   it "年齢20:ユーザーの名前が取得できること" do
+  #     user = User.new(name: "satou", age: 30)
+  #     expect(user.disp_name).to eq "satouさん"
+  #   end
+  
+    let(:params) {{name: "satou"}}
+    it "年齢1:ユーザーの名前が取得できること" do
+      params.merge!(age: 1)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouちゃん"
+    end
+    it "年齢15:ユーザーの名前が取得できること" do
+      params.merge!(age: 15)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouくん"
+    end
+    it "年齢20:ユーザーの名前が取得できること" do
+      params.merge!(age: 20)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouさん"
+    end
+  end
+end
